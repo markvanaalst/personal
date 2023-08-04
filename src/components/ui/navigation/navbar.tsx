4c4091fdeba4ce027bf6e3fa7003358586f8589b
Mark van Aalst <markvanaalst@gmail.com>
2023-08-04 17:15:10 +0200

    add menu toggle for mobile

--- src/components/ui/navigation/navbar.tsx
+++ src/components/ui/navigation/navbar.tsx
@@ -25,6 +25,32 @@ const Navbar = () => {
           </span>
         </a>
       </div>
+      {/* navbar toggler */}
+      <input id="nav-toggle" type="checkbox" className="hidden" />
+      <label
+        id="show-button"
+        htmlFor="nav-toggle"
+        className="flex items-center order-2 cursor-pointer md:order-1 md:hidden"
+      >
+        <svg className="h-6 fill-current" viewBox="0 0 20 20">
+          <title>Menu Open</title>
+          <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
+        </svg>
+      </label>
+      <label
+        id="hide-button"
+        htmlFor="nav-toggle"
+        className="items-center order-2 hidden cursor-pointer md:order-1"
+      >
+        <svg className="h-6 currentColor" viewBox="0 0 20 20">
+          <title>Menu Close</title>
+          <polygon
+            points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+            transform="rotate(45 10 10)"
+          />
+        </svg>
+      </label>
+      {/* /navbar toggler */}
       <ul
         id="nav-menu"
         className="order-3 hidden w-full navbar-nav md:order-1 md:flex md:w-auto md:space-x-2"
