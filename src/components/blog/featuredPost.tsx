c0340c1d6277e357b99bdf94b61f5b8f7bc18c85
Mark van Aalst <markvanaalst@gmail.com>
2023-08-17 21:33:46 +0200

    add opacity filter in dark mode

--- src/components/blog/featuredPost.tsx
+++ src/components/blog/featuredPost.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 import Button from '@/components/ui/button'
 import { getPostUrl } from '@/lib/blog'
+import cn from '@/lib/cn'
 
 import type { Post } from '.contentlayer/generated/types'
 
@@ -13,9 +14,9 @@ type PostItemProps = {
 const FeaturedPostItem = ({ post }: PostItemProps): JSX.Element => {
   return (
     <article className="h-48 md:h-96">
-      <div className="absolute w-full overflow-hidden h-48 md:h-96">
+      <div className="absolute w-full h-48 overflow-hidden md:h-96 dark:bg-background">
         <Image
-          alt="Featured image"
+          alt={post.title}
           src={post.image}
           quality={100}
           placeholder="empty"
@@ -23,6 +24,7 @@ const FeaturedPostItem = ({ post }: PostItemProps): JSX.Element => {
           sizes="(max-width: 768px) 100vw,
 								(max-width: 1200px) 50vw,
 								33vw"
+          className={cn('dark:opacity-30')}
           style={{
             objectFit: 'cover',
           }}
@@ -42,7 +44,7 @@ const FeaturedPostItem = ({ post }: PostItemProps): JSX.Element => {
                 </Link>
               </Button>
             )}
-            <h2 className="my-4 md:text-4xl font-medium text-2xl drop-shadow-sm">
+            <h2 className="my-4 text-2xl font-medium md:text-4xl drop-shadow-sm">
               <Link href={`/blog/${getPostUrl(post.slug)}`}>{post.title}</Link>
             </h2>
             <Button variant="link" className="relative">
