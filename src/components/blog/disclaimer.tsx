2fe87691b97043971fd4aecfecba47e1027eda9c
Mark van Aalst <markvanaalst@gmail.com>
2023-10-19 00:11:11 +0200

    remove padding left/right

--- src/components/blog/disclaimer.tsx
+++ src/components/blog/disclaimer.tsx
@@ -4,8 +4,8 @@ const Profile = () => {
   return (
     <Card>
       <CardHeader>The legal stuff</CardHeader>
-      <CardContent className="w-full max-w-xs  text-center ">
-        <p className="px-8 py-2 text-xs">
+      <CardContent className="w-full max-w-xs text-center ">
+        <p className="py-2 text-xs">
           This is a weblog, most commonly referred to as a “blog”. The opinions
           and thoughts expressed here represent my own and are NOT those of my
           employer, any associations, or any other organizations I am a member
