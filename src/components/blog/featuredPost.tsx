5e2b195ddbbc1a1392404000cefb200ce0cfb8e8
Mark van Aalst <mva@sitecore.net>
2023-11-07 17:22:21 +0100

    fix featured post

--- src/components/blog/featuredPost.tsx
+++ src/components/blog/featuredPost.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 import Button from '@/components/ui/button'
 import { getPostUrl } from '@/lib/blog'
-import cn from '@/lib/cn'
 
 import type { Post } from '.contentlayer/generated/types'
 
@@ -14,48 +12,40 @@ type PostItemProps = {
 const FeaturedPostItem = ({ post }: PostItemProps): JSX.Element => {
   return (
     <article className="h-48 md:h-96">
-      <div className="absolute w-full h-48 overflow-hidden md:h-96 dark:bg-background">
-        <Image
-          alt={post.title}
-          src={post.image}
-          quality={100}
-          placeholder="empty"
-          fill
-          sizes="(max-width: 768px) 100vw,
-								(max-width: 1200px) 50vw,
-								33vw"
-          className={cn('dark:opacity-30')}
-          style={{
-            objectFit: 'cover',
-          }}
-        />
-      </div>
-
-      <div className="h-full text-center ">
-        <div className="flex items-center justify-center h-full py-12 bg-white">
-          <div className="px-4">
-            {post.categories && (
-              <Button variant="tertiary" size={'xs'} className="relative">
-                <Link
-                  href={`/blog/category/${post.categories[0]}`}
-                  className="uppercase"
-                >
-                  {post.categories[0]}
-                </Link>
-              </Button>
-            )}
-            <h2 className="my-2 text-xl font-medium md:my-4 md:text-4xl drop-shadow-sm">
-              <Link href={`/blog/${getPostUrl(post.slug)}`}>{post.title}</Link>
-            </h2>
-            <Button variant="outline" size="lg" className="relative">
-              <Link
-                className="uppercase read-more"
-                href={`/blog/${getPostUrl(post.slug)}`}
-              >
-                Read more
-                <span className="sr-only">{post.title}</span>
-              </Link>
-            </Button>
+      <div
+        className="absolute w-full h-48 overflow-hidden md:h-96 dark:bg-background "
+        style={{ backgroundImage: `url(${post.image})` }}
+      >
+        <div className="h-full bg-slate-100/30 dark:bg-slate-950/70">
+          <div className="h-full text-center">
+            <div className="flex items-center justify-center h-full py-12 backdrop-blur-sm ">
+              <div className="px-4">
+                {post.categories && (
+                  <Button variant="tertiary" size={'xs'} className="relative">
+                    <Link
+                      href={`/blog/category/${post.categories[0]}`}
+                      className="uppercase"
+                    >
+                      {post.categories[0]}
+                    </Link>
+                  </Button>
+                )}
+                <h2 className="my-2 text-xl font-medium md:my-4 md:text-4xl drop-shadow-sm">
+                  <Link href={`/blog/${getPostUrl(post.slug)}`}>
+                    {post.title}
+                  </Link>
+                </h2>
+                <Button variant="outline" size="lg" className="relative">
+                  <Link
+                    className="uppercase read-more"
+                    href={`/blog/${getPostUrl(post.slug)}`}
+                  >
+                    Read more
+                    <span className="sr-only">{post.title}</span>
+                  </Link>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
