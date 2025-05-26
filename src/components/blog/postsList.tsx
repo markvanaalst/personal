3af5d686c1889d1b76d5401b570e9a2326da7190
Mark van Aalst <markvanaalst@gmail.com>
2025-05-27 00:54:04 +0200

    fix: add legacyBehavior to Link components for compatibility

--- src/components/blog/postsList.tsx
+++ src/components/blog/postsList.tsx
@@ -8,7 +8,7 @@ export default function PostsList({ posts }: { posts: Post[] }) {
     <ul className="list-disc pl-5 space-y-2">
       {posts.map(({ frontmatter: { title, date, slug, categories }}, index) => (
         <li key={`${slug}-${index}`}>
-          <Link href={`/blog/${categories[0]}/${slug}`}>
+          <Link href={`/blog/${categories[0]}/${slug}`} legacyBehavior>
             {title} <span className="text-sm text-gray-500">({date})</span>
           </Link>
         </li>
