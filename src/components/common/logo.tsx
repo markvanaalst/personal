59894cb2e3f9a348442ed5ac8bc8ef52c66b2c48
Mark van Aalst <markvanaalst@gmail.com>
2025-05-30 00:56:40 +0200

    refactor(logo): remove unused components and clean up imports

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,38 +1,6 @@
-import type { ComponentProps } from 'react'
+import type { ComponentProps } from 'react';
 
-import { cn } from '@/lib/utils'
-
-export const Sun = ({ className, ...rest }: ComponentProps<'svg'>) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className={cn('w-4 h-4', className)}
-      {...rest}
-    >
-      <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-    </svg>
-  )
-}
-
-export const Moon = ({ className, ...rest }: ComponentProps<'svg'>) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className={cn('w-4 h-4', className)}
-      {...rest}
-    >
-      <path
-        fillRule="evenodd"
-        d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
-        clipRule="evenodd"
-      />
-    </svg>
-  )
-}
+import { cn } from '@/lib/utils';
 
 export const LogoIcon = ({ className, ...rest }: ComponentProps<'svg'>) => {
   return (
@@ -67,21 +35,8 @@ export const LogoIcon = ({ className, ...rest }: ComponentProps<'svg'>) => {
         />
       </g>
     </svg>
-  )
-}
-
-export const MenuOpen = ({ className, ...rest }: ComponentProps<'svg'>) => {
-  return (
-    <svg
-      className={cn('fill-current h-6 w-6', className)}
-      viewBox="0 0 20 20"
-      {...rest}
-    >
-      <title>Menu Open</title>
-      <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
-    </svg>
-  )
-}
+  );
+};
 
 export const Logo = ({ className, ...rest }: ComponentProps<'svg'>) => {
   return (
@@ -176,8 +131,8 @@ export const Logo = ({ className, ...rest }: ComponentProps<'svg'>) => {
         />
       </g>
     </svg>
-  )
-}
+  );
+};
 
 export const LogoWhite = ({ className, ...rest }: ComponentProps<'svg'>) => {
   return (
@@ -251,14 +206,7 @@ export const LogoWhite = ({ className, ...rest }: ComponentProps<'svg'>) => {
       </g>
       <g>
         <g>
-          <circle
-            fill="none"
-            stroke="white"
-            strokeMiterlimit={10}
-            cx="20.6"
-            cy="19.8"
-            r="11.2"
-          />
+          <circle fill="none" stroke="white" strokeMiterlimit={10} cx="20.6" cy="19.8" r="11.2" />
           <path
             fill="white"
             d="M14.9,12.3h1.2l3.2,9.1c0.4,1.1,0.8,2.3,1.2,3.4h0.1c0.4-1.1,0.8-2.3,1.2-3.4l3.2-9.1h1.2v14.9h-0.7V16.8
@@ -272,5 +220,5 @@ export const LogoWhite = ({ className, ...rest }: ComponentProps<'svg'>) => {
         />
       </g>
     </svg>
-  )
-}
+  );
+};
